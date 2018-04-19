9cafca6c961b9d2c934ec116adc8aa4218fdf1a9
CSUS\owp-sp03 <jesse.viola@gmail.com>
2018-04-19 16:35:13 -0700

    testing size

---
+++ src/Fifo.java
@@ -0,0 +1,11 @@
+public class Fifo {
+	private int size;
+	//read in the characters one by one, enter them in a linked list
+	//test of the size
+	public void testsize() {
+		System.out.println(size);
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.io.*;
 
 public class Main {
-
 	public static void main(String[] args) {
-		//command line file readin
+		//instantiate the classes
+		Fifo fifo = new Fifo();
+		
+		//command line file read in
 		if(0 < args.length) {
+			int count = 0;
 			String filename = args[0];
 			File file = new File(filename);
 			try {
@@ -13,10 +16,24 @@ public class Main {
 				while(fis.available() > 0) {
 					current = (char) fis.read();
 					System.out.println(current);
+					count++;
 				}
+				//set the size of the data inputted needed for the datastructure
+				fifo.setSize(count);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+		//add command line catch for arg[1] for specifying what algorithm to run
+		if(args[1].equals("FIFO")) {
+			System.out.println("User started FIFO");
+			fifo.testsize();
+		}
+		else if(args[1].equals("LRU")) {
+			System.out.println("User started LRU");
+		}
+		else if(args[1].equals("OPT")) {
+			System.out.println("User started OPT");
 		}
 	}
 	

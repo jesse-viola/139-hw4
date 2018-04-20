03899b2c5f63ff1866381bf8b8e92ff85abf0777
CSUS\owp-sp03 <jesse.viola@gmail.com>
2018-04-19 17:29:26 -0700

    fixed file read

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -6,24 +10,10 @@ public class Main {
 		Fifo fifo = new Fifo();
 		
 		//command line file read in
-		if(0 < args.length) {
-			int count = 0;
-			String filename = args[0];
-			File file = new File(filename);
-			try {
-				FileInputStream fis = new FileInputStream(file);
-				char current;
-				while(fis.available() > 0) {
-					current = (char) fis.read();
-					System.out.println(current);
-					count++;
-				}
-				//set the size of the data inputted needed for the datastructure
-				fifo.setSize(count);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		String filename = args[0];
+		int[] data = readFiles(filename);
+		System.out.println(Arrays.toString(data));
+	
 		//add command line catch for arg[1] for specifying what algorithm to run
 		if(args[1].equals("FIFO")) {
 			System.out.println("User started FIFO");
@@ -37,4 +27,31 @@ public class Main {
 		}
 	}
 	
+	public static int[] readFiles(String file) {
+		try {
+			File f = new File(file);
+			Scanner scanner = new Scanner(f);
+			int counter = 0;
+			//get count of how many ints are in file
+			while(scanner.hasNextInt()) {
+				counter++;
+				scanner.nextInt();
+			}
+			//create array based off count
+			int[] myArray = new int[counter];
+			//reset cursor
+			Scanner scanner1 = new Scanner(f);
+			for(int i = 0; i < myArray.length; i++) {
+				myArray[i] = scanner1.nextInt();
+			}
+			
+			scanner.close();
+			scanner1.close();
+			
+			return myArray;
+			
+		} catch(Exception e) {
+			return null;
+		}
+	}
 }

7e9c66b3e0857f68b40f759a6f06a78d03da1784
CSUS\owp-sp03 <jesse.viola@gmail.com>
2018-04-25 10:06:20 -0700

    FIFO FIXED!!
    almost lost it boys

--- src/Fifo.java
+++ src/Fifo.java
@@ -1,45 +1,58 @@
-import java.util.*;
-
 public class Fifo {
 	int[] data; //just the numbers from the input.txt
+	int[] frames;
+	int[] pages;
+	int faults, count, totalFrames;
 	
 	public void runFifo() {
 		//grab first three of the file which are params for the algorithm
 		int maxPageIndex = data[0];
 		int totalFrames = data[1];
 		int totalRequests = data[2];
-		int fifoPointer = 0;
-		boolean alreadyLoaded = false;
 		System.out.println("max page index is " + maxPageIndex);
 		System.out.println("total frames is " + totalFrames);
 		System.out.println("total requests is " + totalRequests);
-
-		ArrayList<Integer> frames = new ArrayList<Integer>();
+		int num = 0;
+		boolean flag = true;
 		
-		//add items in until totalFrames is reached
-		for(int i = 3; i < data.length; i++) {
-			if(frames.size() != totalFrames) {
-				//check to see if item is in list already
-				int currentFrame = frames.get(fifoPointer);
-				for(int j = 0; j < totalFrames; j++) {
-					if(currentFrame == data[i]) {
-						System.out.println("Page " + data[i] + "already in Frame " + fifoPointer);
-					}
-					else {
-						frames.add(data[i]);
-						System.out.println("Page " + data[i] + "loaded into Frame " + fifoPointer);
-					}
+		int[] pages = new int[totalRequests];
+		int[] frames = new int[totalFrames];
+		
+		//copy page requests into separate array
+		for(int i = 0; i < pages.length; i++) {
+			pages[i] = data[i+3];
+		}
+		
+		//initialize frame array to -1 since 0 is valid
+		for(int i=0; i<totalFrames; i++) {
+			frames[i] = -1;
+		}
+		
+		for(int i=0; i<totalRequests;i++) {
+			flag = true;
+			int page = pages[i];
+			for(int j=0; j<totalFrames; j++) {
+				if(frames[j] == page) {
+					flag = false;
+					break;
 				}
 			}
-			//else the frames are full and we need to FIFO
-			else{
-				System.out.println("Page " + frames.get(fifoPointer) + "unloaded from Frame " + fifoPointer + " Page " + data[i] + "loaded into Frame " + fifoPointer);
-				frames.remove(fifoPointer);
-				frames.add(fifoPointer, data[i]);
-				fifoPointer++;
-				fifoPointer = fifoPointer % totalFrames;
+			//reset frame
+			if(num == totalFrames)
+				num = 0;
+			
+			if(flag) {
+				frames[num] = page;
+				faults++;
+				num++;
 			}
+			
+			System.out.print("frame : ");
+			for(int k=0; k<totalFrames; k++)
+				System.out.print(frames[k]+ " ");
+			System.out.println();
 		}
+		System.out.println("faults = " + faults);
 	}
 	
 	public void setData(int[] data) {

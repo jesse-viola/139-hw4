1aeea1ccd043b4068dd51ec6d01259ae999e3684
DESKTOP-K32NRDE\Jesse <jesse.viola@gmail.com>
2018-04-24 22:16:25 -0700

    1st attempt FIFO
    Need to fix index error

--- src/Fifo.java
+++ src/Fifo.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Fifo {
 	int[] data; //just the numbers from the input.txt
 	
@@ -6,10 +8,40 @@ public class Fifo {
 		int maxPageIndex = data[0];
 		int totalFrames = data[1];
 		int totalRequests = data[2];
+		int fifoPointer = 0;
+		boolean alreadyLoaded = false;
 		System.out.println("max page index is " + maxPageIndex);
 		System.out.println("total frames is " + totalFrames);
 		System.out.println("total requests is " + totalRequests);
+
+		ArrayList<Integer> frames = new ArrayList<Integer>();
+		
+		//add items in until totalFrames is reached
+		for(int i = 3; i < data.length; i++) {
+			if(frames.size() != totalFrames) {
+				//check to see if item is in list already
+				int currentFrame = frames.get(fifoPointer);
+				for(int j = 0; j < totalFrames; j++) {
+					if(currentFrame == data[i]) {
+						System.out.println("Page " + data[i] + "already in Frame " + fifoPointer);
+					}
+					else {
+						frames.add(data[i]);
+						System.out.println("Page " + data[i] + "loaded into Frame " + fifoPointer);
+					}
+				}
+			}
+			//else the frames are full and we need to FIFO
+			else{
+				System.out.println("Page " + frames.get(fifoPointer) + "unloaded from Frame " + fifoPointer + " Page " + data[i] + "loaded into Frame " + fifoPointer);
+				frames.remove(fifoPointer);
+				frames.add(fifoPointer, data[i]);
+				fifoPointer++;
+				fifoPointer = fifoPointer % totalFrames;
+			}
+		}
 	}
+	
 	public void setData(int[] data) {
 		this.data = data;
 	}
